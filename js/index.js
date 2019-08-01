886ffdc5beafb4bdd8acdb053a4632ec4ae9f06d
siqiliu <siqiliu@cn.ibm.com>
2019-08-01 18:44:33 +0800

    update mobile view more

--- js/index.js
+++ js/index.js
@@ -1,90 +1,90 @@
 $(function () {
-      // initialize the wechat popover
-      $("#wechat-link").popover({
-        trigger: "hover",
-        content: '<div id="qrcode"></div>',
-        placement: "bottom",
-        html: true
+  // initialize the wechat popover
+  $("#wechat-link").popover({
+    trigger: "hover",
+    content: '<div id="qrcode"></div>',
+    placement: "bottom",
+    html: true
+  });
+
+  $('#wechat-link').on('show.bs.popover', function () {
+    setTimeout(function () {
+      $('#qrcode').qrcode({
+        text: window.location.href,
+        width: 75,
+        height: 75
       });
+    }, 0);
+  })
 
-      $('#wechat-link').on('show.bs.popover', function () {
-        setTimeout(function () {
-          $('#qrcode').qrcode({
-            text: window.location.href,
-            width: 75,
-            height: 75
-          });
-        }, 0);
-      })
+  $('#opp-nav-tab a').click(function (e) {
+    var tabNeedToShow = ($(this)[0].id).slice(4);
+    history.pushState({
+      title: tabNeedToShow
+    }, "", 'index.html?tab=' + tabNeedToShow);
+  })
 
-      $('#opp-nav-tab a').click(function (e) {
-        var tabNeedToShow = ($(this)[0].id).slice(4);
-        history.pushState({
-          title: tabNeedToShow
-        }, "", 'index.html?tab=' + tabNeedToShow);
-      })
-
-      var tabContent = ["industryHire", "campusHire", "internship"]
-      if (GetParamFromUrl('tab')) {
-        var tabId = GetParamFromUrl('tab');
-        if (tabContent.indexOf(tabId)) {
-          $('#tab-' + tabId).tab('show');
-        }
-      }
+  var tabContent = ["industryHire", "campusHire", "internship"]
+  if (GetParamFromUrl('tab')) {
+    var tabId = GetParamFromUrl('tab');
+    if (tabContent.indexOf(tabId)) {
+      $('#tab-' + tabId).tab('show');
+    }
+  }
 
 
-      //add hammer in carousel
-      var meetOurTeamElement = document.getElementById('meetOurTeam');
-      var hm = new Hammer(meetOurTeamElement);
-      hm.on("swipeleft", function () {
-        $('#Mycarousel').carousel('next');
-      })
-      hm.on("swiperight", function () {
-        $('#Mycarousel').carousel('prev');
-      })
+  //add hammer in carousel
+  var meetOurTeamElement = document.getElementById('meetOurTeam');
+  var hm = new Hammer(meetOurTeamElement);
+  hm.on("swipeleft", function () {
+    $('#Mycarousel').carousel('next');
+  })
+  hm.on("swiperight", function () {
+    $('#Mycarousel').carousel('prev');
+  })
 
-      var becomePartOfElement = document.getElementById('becomePartOf');
-      var hm = new Hammer(becomePartOfElement);
-      hm.on("swipeleft", function () {
-        $('#Mycarousel').carousel('next');
-      })
-      hm.on("swiperight", function () {
-        $('#Mycarousel').carousel('prev');
-      })
+  var becomePartOfElement = document.getElementById('becomePartOf');
+  var hm = new Hammer(becomePartOfElement);
+  hm.on("swipeleft", function () {
+    $('#Mycarousel').carousel('next');
+  })
+  hm.on("swiperight", function () {
+    $('#Mycarousel').carousel('prev');
+  })
 
-      $('#industryHire')
+  $('#industryHire')
 
-      // $.i18n.properties({
-      //   name : 'i18n',
-      //   path : './i18n/', 
-      //   mode : 'map',
-      //   langugage : 'zh'
-      //   });
-      //initialize out teams module
-      var pageSize = $(window).width();
-      var showCardNumInCom = 4;
-      var indicatorNumArray;
+  // $.i18n.properties({
+  //   name : 'i18n',
+  //   path : './i18n/', 
+  //   mode : 'map',
+  //   langugage : 'zh'
+  //   });
+  //initialize out teams module
+  var pageSize = $(window).width();
+  var showCardNumInCom = 4;
+  var indicatorNumArray;
 
-      function generateTeamIndicator() {
-        if ($(window).width() < 768) {
-          indicatorNumArray = teams;
-        } else {
-          let teamNum = teams.length;
-          let indicatorNum = Math.ceil(teamNum / showCardNumInCom);
-          indicatorNumArray = new Array(indicatorNum).join(',').split(',');
-        }
-        let teamCarouselIndicator = ``;
-        indicatorNumArray.forEach((item, index) => {
-          teamCarouselIndicator = teamCarouselIndicator + `<li data-target="#carouselMeetOurTeam" data-slide-to="${index}" ${index === 0? 'class="active"' : ''}></li>`
-        })
-        $('#meetOurTeamIndicator').append(teamCarouselIndicator);
-      }
+  function generateTeamIndicator() {
+    if ($(window).width() < 768) {
+      indicatorNumArray = teams;
+    } else {
+      let teamNum = teams.length;
+      let indicatorNum = Math.ceil(teamNum / showCardNumInCom);
+      indicatorNumArray = new Array(indicatorNum).join(',').split(',');
+    }
+    let teamCarouselIndicator = ``;
+    indicatorNumArray.forEach((item, index) => {
+      teamCarouselIndicator = teamCarouselIndicator + `<li data-target="#carouselMeetOurTeam" data-slide-to="${index}" ${index === 0? 'class="active"' : ''}></li>`
+    })
+    $('#meetOurTeamIndicator').append(teamCarouselIndicator);
+  }
 
-      function generateTeamInnerCard() {
-        let teamCard = [];
-        var cardWidth = 12 / showCardNumInCom;
-        teams.forEach((team, index) => {
-          let item = `
+  function generateTeamInnerCard() {
+    let teamCard = [];
+    var cardWidth = 12 / showCardNumInCom;
+    teams.forEach((team, index) => {
+      let item = `
       <div class="col-sm-12 col-md-${cardWidth} col-lg-${cardWidth}">
         <div class="task-card" style="overflow:hidden">
           <img src="img/${team.icon}.png" align="top" class="ibm-padding-bottom-1">
@@ -93,51 +93,51 @@ $(function () {
         </p>
         </div>
       </div>`
-          teamCard.push(item);
-        })
-        var teamInner = ``;
-        if ($(window).width() < 768) {
-          indicatorNumArray.forEach((item, index) => {
-            teamInner = teamInner + `<div  ${index === 0? 'class="carousel-item active cio-career-padding-bottom-normal"' : 'class="carousel-item cio-career-padding-bottom-normal"'}>` + teamCard[index] + `</div>`
-          })
-        } else {
-          var currentCardIndex = 0;
-          var index1 = 0;
-          indicatorNumArray.forEach((item, index) => {
-            currentCardIndex = index1;
-            var teamCardInner = ``;
-            for (index1; index1 < currentCardIndex + 4 && index1 < teams.length; index1++) {
-              teamCardInner = teamCardInner + teamCard[index1]
-            }
-            teamInner = teamInner + `<div  ${index === 0? 'class="carousel-item active cio-career-padding-bottom-normal"' : 'class="carousel-item cio-career-padding-bottom-normal"'}> <div class="row">` + teamCardInner +
-              `</div></div>`;
-          })
+      teamCard.push(item);
+    })
+    var teamInner = ``;
+    if ($(window).width() < 768) {
+      indicatorNumArray.forEach((item, index) => {
+        teamInner = teamInner + `<div  ${index === 0? 'class="carousel-item active cio-career-padding-bottom-normal"' : 'class="carousel-item cio-career-padding-bottom-normal"'}>` + teamCard[index] + `</div>`
+      })
+    } else {
+      var currentCardIndex = 0;
+      var index1 = 0;
+      indicatorNumArray.forEach((item, index) => {
+        currentCardIndex = index1;
+        var teamCardInner = ``;
+        for (index1; index1 < currentCardIndex + 4 && index1 < teams.length; index1++) {
+          teamCardInner = teamCardInner + teamCard[index1]
         }
-        $('#meetOurTeamInner').append(teamInner)
-      }
+        teamInner = teamInner + `<div  ${index === 0? 'class="carousel-item active cio-career-padding-bottom-normal"' : 'class="carousel-item cio-career-padding-bottom-normal"'}> <div class="row">` + teamCardInner +
+          `</div></div>`;
+      })
+    }
+    $('#meetOurTeamInner').append(teamInner)
+  }
 
-      function GetParamFromUrl(name) {
-        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-        var regexS = "[\/|\?|\&]" + name + "=([^&#]*)";
-        var regex = new RegExp(regexS, "g");
-        var results = regex.exec(window.location.href);
-        if (results === null) {
-          return "";
-        }
-        return results[1];
-      }
+  function GetParamFromUrl(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\/|\?|\&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS, "g");
+    var results = regex.exec(window.location.href);
+    if (results === null) {
+      return "";
+    }
+    return results[1];
+  }
 
-      var hireTab = ['industryHire', 'campusHire', 'internship']
+  var hireTab = ['industryHire', 'campusHire', 'internship']
 
-      function generateJobOpp() {
-        hireTab.forEach((item, index) => {
-          $('#' + item).empty()
-        })
-        if ($(window).width() >= 768) {
-          for (var j = 0; j < hireTab.length; j++) {
-            // initialize job opportunities
-            jobs[hireTab[j]].forEach((job, index) => {
-              $('#' + hireTab[j]).append(`
+  function generateJobOpp() {
+    hireTab.forEach((item, index) => {
+      $('#' + item).empty()
+    })
+    if ($(window).width() >= 768) {
+      for (var j = 0; j < hireTab.length; j++) {
+        // initialize job opportunities
+        jobs[hireTab[j]].forEach((job, index) => {
+          $('#' + hireTab[j]).append(`
     <div class="job-card row">
       <div class="industryHireImg col-sm-0 col-md-2 col-lg-2">
         <img src="img/Card-icon-${job.type}-Web.png" style="width:100%;height:100%"></img>
@@ -150,24 +150,25 @@ $(function () {
             <span>${job.experience}</span>
         </p>
         <p class="ibm-padding-bottom-1">${job.description}</p>
-        <p style="position:absolute;left;0;bottom:25px;">
+        <p style="position:absolute;bottom:25px;">
             <a class="view-more" href="details.html?category=industry&index=${index}">View More -></a>
         </p>
       </div>
     </div>
     `);
-            });
-          }
-        } else {
-          for (var j = 0; j < hireTab.length; j++) {
-            // initialize job opportunities
-            jobs[hireTab[j]].forEach((job, index) => {
-              $('#' + hireTab[j]).append(`
+        });
+      }
+    } else {
+      for (var j = 0; j < hireTab.length; j++) {
+        // initialize job opportunities
+        jobs[hireTab[j]].forEach((job, index) => {
+          $('#' + hireTab[j]).append(`
             <div class="job-card">
               <div class="industryHireImg industryHireLeft">
                <img src="img/Card-icon-${job.type}-Mobile.png" style="height:100%">
               </div>
-              <div class="job-card-content industryHireRight style="overflow:hidden">
+              <div class="industryHireRight style="overflow:hidden">
+              <div style="position:relative" class="job-card-content">
               <p class="cio-career-h2">${job.position}</p>
                 <p>
                   <span>${job.location}</span> |
@@ -175,31 +176,32 @@ $(function () {
                   <span>${job.experience}</span>
                 </p>
                 <p class="ibm-padding-bottom-1">${job.description}</p>
-                <p>
-                  <a class="view-more" href="details.html?category=industry&index=${index}" style="bottom:2rem">View More -></a>
+                <p style="position:absolute;bottom:25px;">
+                  <a class="view-more" href="details.html?category=industry&index=${index}">View More -></a>
                 </p>
+                </div>
               </div>
             </div>
         `);
-            });
-          }
-        }
+        });
       }
+    }
+  }
 
-        generateTeamIndicator();
-        generateTeamInnerCard();
-        generateJobOpp()
+  generateTeamIndicator();
+  generateTeamInnerCard();
+  generateJobOpp()
 
-        $(window).resize(function () {
-          if ((pageSize < 768 && $(window).width() >= 768) || pageSize >= 768 && $(window).width() < 768) {
-            pageSize = $(window).width();
-            $('#meetOurTeamInner').empty();
-            $('#meetOurTeamIndicator').empty();
-            generateTeamIndicator();
-            generateTeamInnerCard();
-            generateJobOpp();
-          }
-        })
+  $(window).resize(function () {
+    if ((pageSize < 768 && $(window).width() >= 768) || pageSize >= 768 && $(window).width() < 768) {
+      pageSize = $(window).width();
+      $('#meetOurTeamInner').empty();
+      $('#meetOurTeamIndicator').empty();
+      generateTeamIndicator();
+      generateTeamInnerCard();
+      generateJobOpp();
+    }
+  })
 
 
-      });
+});
