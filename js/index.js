480ddf2178e9b4286f86c0617a0a09911824543b
siqiliu <siqiliu@cn.ibm.com>
2019-08-07 17:10:45 +0800

    update tab format and icon size

--- js/index.js
+++ js/index.js
@@ -95,7 +95,7 @@ $(function () {
       <div class="col-sm-12 col-md-${cardWidth} col-lg-${cardWidth}">
       <div class="team-card-pad">
         <div class="task-card" style="overflow:hidden">
-          <img src="img/${team.icon}.png" align="top" style="height:3.75rem">
+          <img src="img/${team.icon}.png" align="top" class="icon-size">
           <p class="ibm-bold teamName-top-pad" style="padding-bottom:5px">${team.teamName}</p>
           <p>${team.teamDetail}
         </p>
